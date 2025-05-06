e1868ee2d179d49c392b2a8dfa878d7600038a84
ziyi-x073 <ziyixie073@gmail.com>
2025-05-06 15:46:06 +0800

    Update FunctionsVariables.py

--- CS50's Introduction to Programming with Python/FunctionsVariables.py
+++ CS50's Introduction to Programming with Python/FunctionsVariables.py
@@ -4,7 +4,7 @@ name = input("What's your name? ").strip().title()
 # Split user's name into first name and last name
 first, last = name.split(" ")
 
-# Say hello to user
+# Say hello to user f"{变量}"是在控制格式，让大括号中的内容以变量格式呈现，而不是“”这种文本的hard code
 print(f"hello,{first}")
 
 ---
@@ -16,5 +16,17 @@ y = float(input("What's y? "))
 # Create a rounded result
 z = round(x + y)
 
-# Print the formatted result
+# Print the formatted result 整数位隔三位一个逗号，比如1000会输出1,000
 print(f"{z:,}")
+
+---
+
+# Get the user's input
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+
+# Calculate the result
+z = x / y
+
+# Print the result 小数点后面省略两位，比如0.666666会输出0.67
+print(f"{z:.2f}")
