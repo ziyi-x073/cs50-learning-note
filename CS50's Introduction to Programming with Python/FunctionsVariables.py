c7099935f9cba861c8ba88e95c1f8d4c421e8ce3
ziyi-x073 <ziyixie073@gmail.com>
2025-05-06 15:32:55 +0800

    Update FunctionsVariables.py

--- CS50's Introduction to Programming with Python/FunctionsVariables.py
+++ CS50's Introduction to Programming with Python/FunctionsVariables.py
@@ -6,3 +6,15 @@ first, last = name.split(" ")
 
 # Say hello to user
 print(f"hello,{first}")
+
+---
+
+# Get the user's input
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+
+# Create a rounded result
+z = round(x + y)
+
+# Print the formatted result
+print(f"{z:,}")
